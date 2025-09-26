76e3325d4b620ba918195a41766250c755e1830a
Paulo <pescanellasotero@gmail.com>
2025-09-26 11:29:00 +0200

    1.6. Usamos una array para almacenar una serie de elementos para posteriormente, mostrarlos mediante una lista

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -3,30 +3,23 @@
 <head>
     <meta charset="UTF-8">
     <title>Demo</title>
-    <style>
-        body{
-            display : grid;
-            place-items: center;
-            height: 100vh;
-            margin 0;
-            font-family: sans-serif;
-        }
-    </style>
 </head>
 <body>
-    <?php 
-        $name = "Dark Matter";
-        $read = true;
+    <h1>Recommended Books</h1>
 
-        if($read){
-            $message = "You have read $name";
-        }else{
-            $message = "You have not read $name";
-        }
+    <?php
+        $books = [
+            "Do Androids Dream of Electric Sheep",
+            "The Langoliers",
+            "Hail Mary"
+        ];
     ?>
-    
-    <h1>
-        <?php echo $message; ?>
-    </h1>
+
+    <ul>
+        <?php foreach ($books as $book){
+            echo "<li>$book</li>";
+        }
+        ?>
+    </ul>
 </body>
 </html>
