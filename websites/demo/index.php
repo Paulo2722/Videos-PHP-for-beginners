0c37e88c14cb0b4dd19d3385d243e66581436849
Paulo <pescanellasotero@gmail.com>
2025-09-26 11:55:29 +0200

    1.8. Añadimos una condición con la que solo mostraremos los libros escritos por x persona que esten almacenados en la array

--- websites/demo/index.php
+++ websites/demo/index.php
@@ -12,12 +12,20 @@
             [
                 'name' => 'Do Androids Dream of Electric Sheep',
                 'author' => 'Phipips K.Dick',
+                'releaseYear' => 1968,
                 'purchaaseURL' => 'http://example.com'
                 
             ],
             [
                 'name' => 'Hail Mary',
                 'author' => 'Andy Weir',
+                'releaseYear' => 2021,
+                'purchaaseURL' => 'http://example.com'
+            ],
+            [
+                'name' => 'The Martian',
+                'author' => 'Andy Weir',
+                'releaseYear' => 2011,
                 'purchaaseURL' => 'http://example.com'
             ]
         ];
@@ -25,11 +33,13 @@
 
     <ul>
         <?php foreach ($books as $book) : ?>
-            <li>
-                <a href="<?= $book['purchaseURL'] ?>">
-                    <?= $book['name']; ?>
-                </a>
-            </li>
+            <?php if ($book['author'] === 'Andy Weir') : ?>
+                <li>
+                    <a href="<?= $book['purchaseURL'] ?>">
+                        <?= $book['name']; ?> (<?= $book['releaseYear'] ?>) - By <?= $book['author'] ?>
+                    </a>
+                </li>
+            <?php endif; ?>
         <?php endforeach; ?>
     </ul>
 </body>
