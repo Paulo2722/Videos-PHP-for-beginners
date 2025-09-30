af841ae2285733d651dd3057f96cf6bc395f5eda
Paulo <pescanellasotero@gmail.com>
2025-09-30 10:24:44 +0200

    2.1. Modificamos la plantilla original y creamos una serie de nuevos archivos php

--- websites/demo/about.view.php
+++ websites/demo/about.view.php
@@ -109,7 +109,7 @@
   </header>
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-      <p>Hello. Welcome to the home page</p>
+      <p>Now you are on the About page.</p>
     </div>
   </main>
 </div>
--- websites/demo/index.php
+++ websites/demo/index.php
@@ -1,16 +1,4 @@
 <?php
 
-$business = [
-    'name' => 'Laracasts',
-    'cost' => 15,
-    'categories' => ["Testing", "PHP", "JavaScript"]
-];
-
-function register($user){
-    //Create the user record in the db
-    //Log them in
-    //Sendcome a welcome email
-    //Redirect to their new dashboard
-}
 
 require "index.view.php";
