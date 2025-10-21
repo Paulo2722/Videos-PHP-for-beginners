98ba05efaa1eb1aa5a2c44dbb51a2536858cb1f2
Paulo <pescanellasotero@gmail.com>
2025-10-21 10:29:22 +0200

    3.3. Añadimos un control de errores/seguridad en el que si se accede a una nota con un usuario incorrecto, este no podrá ver su contenido y saltará un error

--- websites/demo/views/note.view.php
+++ websites/demo/views/note.view.php
@@ -5,8 +5,9 @@
   <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
       <p class="mb-6">
-        <a href="index.php/notes" class="text-blue-500 underline">go back...</a>  
-      <?= $note['body'] ?></p>
+        <a href="index.php/notes" class="text-blue-500 underline">go back...</a>
+</p>
+     <p><?= $note['body'] ?></p>
   </div>
   </main>
 
