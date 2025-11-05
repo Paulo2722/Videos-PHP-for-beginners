eda60fc05864aae11b1cea39c0d4cdce17b81ab8
Paulo <pescanellasotero@gmail.com>
2025-11-05 11:09:34 +0100

    4.8. Añado la funcionalidad de poder editar las notas ya creadas

---
+++ websites/demo/controllers/notes/update.php
@@ -0,0 +1,40 @@
+<?php
+
+use Core\App;
+use Core\Database;
+use Core\Validator;
+
+$db = App::resolve(Database::class);
+
+//find the corresponding note
+$currentUserId = 1;
+$note = $db->query('select * from notes where id = :id', [
+    'id' => $_POST['id']
+])->findOrFail();
+
+//authorize that current user can edit the note
+authorize($note['user_id'] === $currentUserId);
+
+//validate the form
+$errors = [];
+
+if (! Validator::string($_POST['body'], 1, 1000)) {
+    $errors['body'] = 'A body of no more than 1,000 characters is required.';
+}
+
+//if no validation errors, update the record in the notes database table
+if (count($errors)){
+    return view('notes/edit.view.php', [
+        'heading' => 'Edit note',
+        'errors' => $errors,
+        'note' => $note
+    ]);
+}
+
+$db->query('update notes set body = :body where id = :id', [
+    'id' => $_POST['id'],
+    'body' => $_POST['body']
+]);
+
+header('location: /index.php/notes');
+die();
